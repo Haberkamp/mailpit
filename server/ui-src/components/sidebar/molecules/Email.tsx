602e4bc9434a2c57da5309b02bfbfc19bf9bdbfc
Nils Haberkamp <haberkamp.n@gmail.com>
2025-04-20 17:30:15 +0200

    chore: add <Email /> component

---
+++ server/ui-src/components/sidebar/molecules/Email.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Text, ListBoxItem } from "react-aria-components";
+
+interface Props {
+  senderName: string;
+  subject: string;
+  description: string;
+}
+
+export function Email({ senderName, subject, description }: Props) {
+  return (
+    <ListBoxItem className="p-4 outline-blue-500 rounded-lg cursor-pointer hover:bg-gray-200">
+      <Text className="block font-semibold">{senderName}</Text>
+
+      <Text className="block">{subject}</Text>
+
+      <Text className="text-gray-500 text-sm line-clamp-2">{description}</Text>
+    </ListBoxItem>
+  );
+}
