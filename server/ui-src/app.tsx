4e2ca4cb30975044981cff8b16b2feb36c867aac
Nils Haberkamp <haberkamp.n@gmail.com>
2025-04-20 17:30:51 +0200

    chore: display dummy emails

--- server/ui-src/app.tsx
+++ server/ui-src/app.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Logo } from "./components/brand/atoms/Logo";
+import { Header, ListBox, Text } from "react-aria-components";
+import { Email } from "./components/sidebar/molecules/Email";
 
 export function App() {
   return (
@@ -8,7 +10,27 @@ export function App() {
         <Logo aria-hidden />
       </header>
 
-      <aside></aside>
+      <aside>
+        <Header className="text-xl font-semibold text-gray-900">Inbox</Header>
+
+        <Text className="block text-sm">{24} unread emails</Text>
+
+        <div className="pt-4" />
+
+        <ListBox className="-ms-4">
+          <Email
+            senderName="Amazon.com"
+            subject="Your order has shipped"
+            description="You've ordered a book about Antifragility by Nassim Nicholas Taleb"
+          />
+
+          <Email
+            senderName="John Doe"
+            subject="Hello, world!"
+            description="Hello, how are you?"
+          />
+        </ListBox>
+      </aside>
 
       <main className="bg-white rounded-lg border border-gray-300"></main>
     </div>
